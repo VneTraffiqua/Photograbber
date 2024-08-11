2da00b24d33011880148da785cb4e444453fe85f
VneTraffiqua <31010342+VneTraffiqua@users.noreply.github.com>
2024-08-11 08:19:07 -0700

    Update server.py

--- async-service/server.py
+++ async-service/server.py
@@ -1,27 +1,23 @@
 from aiohttp import web
 import asyncio
 import aiofiles
-import datetime
 import os
 import logging
-
-
-THROTTLING_TICS = 0
+import argparse
 
 
 logging.basicConfig(
     format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
-    level=logging.DEBUG,
+    level=logging.INFO,
     filename=u'photo_archive_log.log'
 )
 
 
-INTERVAL_SECS = 1
-
-
 async def archive(request):
+    global args
+
     archive_hash = request.match_info.get('archive_hash')
-    if not os.path.exists(os.path.join('test_photos', archive_hash)):
+    if not os.path.exists(os.path.join(args.path, archive_hash)):
         raise web.HTTPNotFound(text=f'404: Архив {archive_hash} не существует или был удален')
     response = web.StreamResponse()
     response.headers['Content-Disposition'] = f'attachment; filename="{archive_hash}.zip"'
@@ -33,16 +29,21 @@ async def archive(request):
         'zip', '-r', '-', f'{archive_hash}',
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
-        cwd='test_photos'
+        cwd=args.path
     )
     try:
         while not proc.stdout.at_eof():
             archive_data = await proc.stdout.read(100000)
             logging.info(msg=f'Sending archive chunk {archive_hash}')
-            await asyncio.sleep(THROTTLING_TICS)
+            await asyncio.sleep(args.sleep)
             await response.write(archive_data)
+    except ConnectionResetError:
+        logging.error(msg='ConnectionResetError. Download was interrupted')
+    except BaseException:
+        logging.error(msg='SystemExit. Download was interrupted')
     finally:
-        logging.error(msg='Download was interrupted')
+        proc.kill()
+        await proc.communicate()
     return response
 
 
@@ -52,32 +53,28 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
-async def uptime_handler(request):
-    response = web.StreamResponse()
-
-    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
-    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
-    response.headers['Content-Type'] = 'text/html'
-
-    # Отправляет клиенту HTTP заголовки
-    await response.prepare(request)
-
-    while True:
-        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
-
-        # Отправляет клиенту очередную порцию ответа
-        await response.write(message.encode('utf-8'))
-
-        await asyncio.sleep(INTERVAL_SECS)
-
-
 if __name__ == '__main__':
-    data = b''
+    parser = argparse.ArgumentParser(
+        prog='Photograbber',
+        description='Microservice for downloading files',
+        add_help=True
+    )
+
+    parser.add_argument('-l', '--logging', action='store_true', default=False, help='logs usage')
+    parser.add_argument('-s', '--sleep', type=int, default=0, help='response delay')
+    parser.add_argument('-p', '--path', type=str, default='test_photos', help='path to the photo folder')
+    args = parser.parse_args()
+    print(args)
+    # if args.logging:
+    #     logging.basicConfig(
+    #         format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+    #         level=logging.INFO,
+    #         filename=u'photo_archive_log.log'
+    #     )
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archive),
-        web.get('/archive/7kna', uptime_handler)
     ])
     web.run_app(app)
