3b8610c7d416c8f8e5c86d398f7c38acc1c55e7d
VneTraffiqua <31010342+VneTraffiqua@users.noreply.github.com>
2024-09-05 07:07:41 -0700

    Deleted garbage

--- main.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-archive = subprocess.Popen(["zip", "-r", "-", "media"], stdout=subprocess.PIPE)
-data = archive.communicate()
-
-with open('media_test.zip', "wb") as file:
-    file.write(bytes(data[0]))
